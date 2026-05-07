1ba8b1a795ebbf77c1337a80877b4eca3f2174b7
Arthur <xododomar@hotmail.com>
2026-05-07 17:54:42 -0300

    Add files via upload

--- Hardwareshop/Main/backend/src/main/java/org/example/Main.java
+++ Hardwareshop/Main/backend/src/main/java/org/example/Main.java
@@ -1,40 +1,95 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 
-class Produto{
+class Produto {
     String nome;
     double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " R$ " + preco;
+    }
 }
-public class Main{
-    public static void main (String[]args) {
+
+// Monitor
+class Monitor extends Produto {
+    int hz;
+
+    public Monitor(String nome, double preco, int hz) {
+        super(nome, preco);
+        this.hz = hz;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + hz + "Hz R$ " + preco;
+    }
+}
+
+// Mouse
+class Mouse extends Produto {
+    int dpi;
+
+    public Mouse(String nome, double preco, int dpi) {
+        super(nome, preco);
+        this.dpi = dpi;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + dpi + "Dpi R$ " + preco;
+    }
+}
+
+// GPU
+class GPU extends Produto {
+    int Vram;
+
+    public GPU(String nome, double preco, int Vram) {
+        super(nome, preco);
+        this.Vram = Vram;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + Vram + "Gb R$ " + preco;
+    }
+}
+
+//CPU
+class CPU extends Produto {
+
+    public CPU(String nome, double preco) {
+        super(nome, preco);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + " R$ " + preco;
+    }
+}
+
+//Lista dos produtos
+public class Main {
+    public static void main(String[] args) {
 
         ArrayList<Produto> produtos = new ArrayList<>();
 
-        Produto mouse = new Produto();
-        mouse.nome = "Mouse Gamer";
-        mouse.preco = 149.99;
-
-        Produto teclado = new Produto();
-        teclado.nome = "Teclado Mecânico";
-        teclado.preco = 299.99;
-
-        Produto monitor = new Produto();
-        monitor.nome = "Monitor";
-        monitor.preco = 999.99;
-
-        Produto gabinete  = new Produto();
-        gabinete.nome = "Gabinete";
-        gabinete.preco = 349.99;
-
-        produtos.add(mouse);
-        produtos.add(teclado);
-        produtos.add(monitor);
-        produtos.add(gabinete);
-
+        produtos.add(new Monitor("Monitor Gamer", 999.99, 144));
+        produtos.add(new Monitor("Monitor Gamer", 1599.99, 240));
+        produtos.add(new Mouse("Mouse", 249.99, 16000));
+        produtos.add(new GPU("RTX 5060 Ti", 2999.99, 8));
+        produtos.add(new GPU("RX 9070Xt", 4499.99, 16));
+        produtos.add(new CPU("AMD Ryzen 9 9950X3D", 3999.99));
+        produtos.add(new CPU("i9-14900k", 2899.99));
 
         for (Produto p : produtos) {
-            System.out.println(p.nome + " R$ " + p.preco);
+            System.out.println(p);
         }
     }
 }
